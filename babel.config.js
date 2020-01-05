a4c7f29b04e0acc128509ca7085e666c196a150d
Simon Bösch <simon-andreas.boesch@hotmail.com>
2020-01-05 17:22:10 +0100

    Add Uikit3

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,5 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ],
-  dev: {
-    proxyTable: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true
-      }
-    },
-  }
+  ]
 }
